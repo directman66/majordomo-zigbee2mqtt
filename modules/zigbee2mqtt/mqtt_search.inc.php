146bb90891835d707ce6350febdee0e2d746aea8
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-09 00:45:02 +0500

    Update mqtt_search.inc.php

--- modules/zigbee2mqtt/mqtt_search.inc.php
+++ modules/zigbee2mqtt/mqtt_search.inc.php
@@ -15,6 +15,59 @@
    $out['TITLE']=$title;
   }
 
+
+
+   //deviles
+//  $res=SQLSelect("SELECT * FROM zigbee2mqtt_devices WHERE $qry ORDER BY ".$sortby_mqtt);
+//if ($this->view_mode==''||$this->view_mode=='device')
+
+  $res=SQLSelect("SELECT * FROM zigbee2mqtt_devices ");
+  if ($res[0]['ID']) {
+   if (!$out['TREE']) {
+    paging($res, 50, $out); // search result paging
+   }
+   $total=count($res);
+   for($i=0;$i<$total;$i++) {
+    // some action for every record if required
+    //$tmp=explode(' ', $res[$i]['UPDATED']);
+    //$res[$i]['UPDATED']=fromDBDate($tmp[0])." ".$tmp[1];
+//    $res[$i]['VALUE']=str_replace('":','": ',$res[$i]['VALUE']);
+
+    if ($res[$i]['TITLE']==$res[$i]['PATH'] && !$out['TREE']) $res[$i]['PATH']='';
+   }
+   $out['DEVICES']=$res;
+
+
+  }
+
+//$vm=$res;
+// echo "<script type='text/javascript'>";
+// echo "alert('$vm');";
+// echo "</script>";
+
+
+
+
+  $out['LOCATIONS']=SQLSelect("SELECT * FROM locations ORDER BY TITLE");
+  $out['ZIGBEE2MQTTDEV']=SQLSelect("SELECT @rownum := @rownum + 1 AS ID,t.* FROM (SELECT distinct SPLIT_STRING(TITLE, '/', 2) TITLE FROM zigbee2mqtt) t, (SELECT @rownum := 0) r");
+
+
+
+  global $dev_id;
+
+// echo "<script type='text/javascript'>";
+// echo "alert('$dev_id');";
+// echo "</script>";
+
+
+
+  if ($dev_id!='') {
+   $qry.=" AND (TITLE LIKE '%".DBSafe($dev_id)."%' OR VALUE LIKE '%".DBSafe($dev_id)."%' OR PATH LIKE '%".DBSafe($dev_id)."%')";
+   $out['ZIGBEE2MQTTDEV']=$dev_id;
+  }
+
+
+
   global $location_id;
   if ($location_id) {
    $qry.=" AND LOCATION_ID='".(int)$location_id."'";
@@ -76,7 +129,21 @@
   if ($out['TREE']) {
    $sortby_mqtt='PATH';
   }
-  $res=SQLSelect("SELECT * FROM zigbee2mqtt WHERE $qry ORDER BY ".$sortby_mqtt);
+//echo $this->view_mode;
+//$vm=$this->view_mode;
+// echo "<script type='text/javascript'>";
+// echo "alert('$vm');";
+// echo "</script>";
+
+
+if ($this->view_mode=='view_mqtt'){
+
+$vm=$this->id;
+// echo "<script type='text/javascript'>";
+// echo "alert('$vm');";
+// echo "</script>";
+
+  $res=SQLSelect("SELECT * FROM zigbee2mqtt WHERE dev_id=".$this->id);
   if ($res[0]['ID']) {
    if (!$out['TREE']) {
     paging($res, 50, $out); // search result paging
@@ -97,8 +164,13 @@
    }
 
   }
+}
 
 
-  $out['LOCATIONS']=SQLSelect("SELECT * FROM locations ORDER BY TITLE");
+
+
+
+
+
 
 ?>
