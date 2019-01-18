9b5b63121cde820ab47f145d3bc5a8d0252baa53
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-18 15:04:53 +0500

    Update map.inc.php

--- modules/zigbee2mqtt/map.inc.php
+++ modules/zigbee2mqtt/map.inc.php
@@ -1,30 +1,61 @@
-<?php
+<?php          
 //$maparray=SQLSelectOne ('select * from zigbee2mqtt where value like "digraph%"');
-$maparray=SQLSelectOne ('select * from zigbee2mqtt where path like "zigbee2mqtt/bridge/networkmap/raw"');
+$maparray=SQLSelect('SELECT *  FROM (select zigbee2mqtt_devices.ID DEVID, zigbee2mqtt_devices.* from zigbee2mqtt_devices )zigbee2mqtt_devices LEFT JOIN zigbee2mqtt_devices_list ON zigbee2mqtt_devices_list.zigbeeModel=zigbee2mqtt_devices.MODEL ');
 
-$out['map_array']=$maparray['VALUE'];
+//$out['map_array']=$maparray['VALUE'];
+ $nodes="";
+$egdes="";
+    $total = count($maparray);
+    for ($i=0;$i<$total;$i++) {
 
-$nodess=json_decode($maparray['VALUE'],true);
+if (!$maparray[$i]['ID']) {$idd='0';} else {$idd=$maparray[$i]['ID'];}
+//   $nodes .= "{id: ".$idd.", font:{size:10}, size:40,   label: '".$maparray[$i]['description']."',  group: 0,  shape: 'circularImage', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', shapeProperties:{borderDashes:[5,5]},},";
+   $nodes.= "{id: ".$idd.", font:{size:10}, size:40,   label: '".$maparray[$i]['description']."',  group: 0,  shape: 'circularImage', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', },";
+ if ($idd!="0")   $edges.= "{from: 0, to: ".$idd."},";
+}    
+
+//   $edges.= '{"id": "7","from": "7","to": "0},{"id": "9","from": "9","to": "0" },{"id": "4","from": "2","to": "0"},{"id": "5","from": "3","to": "0" },';
+//   $edges.= '{from: 0, to: 7},';
+
+
+
+$out['map_array']=$nodes;
+//$out['EDGES']=$edges;
+
+
+//$nodess=json_decode($maparray['VALUE'],true);
 
 //debmes ($maparray['VALUE'],'zigbee2mqtt');
 //debmes ($nodess,'zigbee2mqtt');
-/*
-        foreach($nodess as $k=>$v) { 
+
+//        foreach($nodess as $k=>$v) { 
 //        {id: 11, font:{size:30}, size:40, label: 'coordinator', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties: {borderDashes:[15,5]}},	
-	if (is_array($v))
- 	        foreach($v as $kk=>$vv)
-		{}
+//	if (is_array($v))
+// 	        foreach($v as $kk=>$vv)
+//		{}
 
-echo $k.":".$v;
- }
+//echo $k.":".$v;
+// }
 
-*/
+/*
   $nodes = <<<EOD
         {id: 1, font:{size:30},          label: 'circle',  shape: 'circle' , shapeProperties:{borderDashes:[5,5]}},
-        {id: 11, font:{size:30}, size:40, label: 'coordinator', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties: {borderDashes:[15,5]}},	
+        {id: 12, font:{size:30}, size:40, label: 'coordinator', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties: {borderDashes:[15,5]}},	
 EOD;
 
-$out['NODES']=$nodes
+//        {id: 7, font:{size:30}, label: 'Aqara single key wired wall switch', shape: 'circle', image: '/templates/zigbee2mqtt/img/QBKG04LM.jpg', shapeProperties:{borderDashes:[5,5]},
+//        {id: 7, font:{size:30}, , size:40, label: 'Aqara single key wired wall switch', shape: 'circle', image: '/templates/zigbee2mqtt/img/QBKG04LM.jpg', shapeProperties:{borderDashes:[5,5]}},
+  $nodes = <<<EOD
+        {id: 7, font:{size:30},  size:40, label: 'Aqara single key wired wall switch', shape: 'circle', image: '/templates/zigbee2mqtt/img/QBKG04LM.jpg', shapeProperties:{borderDashes:[5,5]}},
+        {id: 1, font:{size:30}, size:40, label: 'coordinator', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties: {borderDashes:[15,5]}},	
+        {id: 12, font:{size:30}, size:40, label: 'coordinator', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties: {borderDashes:[15,5]}},	
+EOD;
+*/
+//$nodes="{id: 7, font:{size:10}, size:40, label: 'Aqara single key wired wall switch', shape: 'circle', image: '/templates/zigbee2mqtt/img/QBKG04LM.jpg', shapeProperties:{borderDashes:[5,5]}},";
+//$nodes="{id: 7, font:{size:10}, size:40, label: 'Aqara single key wired wall switch', shape: 'circularImage', image: '/templates/zigbee2mqtt/img/cc2531.jpg', shapeProperties:{borderDashes:[5,5]}},";
+
+$out['NODES']=$nodes;
+$out['EDGES']=$edges;
 
 
 
