e17b8bdb4c7071ae6c4ce5443416c88397050a4e
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-02-01 11:16:55 +0500

    Update map.inc.php

--- modules/zigbee2mqtt/map.inc.php
+++ modules/zigbee2mqtt/map.inc.php
@@ -1,6 +1,9 @@
 <?php          
 //$maparray=SQLSelectOne ('select * from zigbee2mqtt where value like "digraph%"');
-$maparray=SQLSelect('SELECT *  FROM (select zigbee2mqtt_devices.ID DEVID, zigbee2mqtt_devices.* from zigbee2mqtt_devices )zigbee2mqtt_devices LEFT JOIN zigbee2mqtt_devices_list ON zigbee2mqtt_devices_list.zigbeeModel=zigbee2mqtt_devices.MODEL ');
+//$maparray=SQLSelect('SELECT *  FROM (select zigbee2mqtt_devices.ID DEVID, zigbee2mqtt_devices.* from zigbee2mqtt_devices )zigbee2mqtt_devices LEFT JOIN zigbee2mqtt_devices_list ON zigbee2mqtt_devices_list.zigbeeModel=zigbee2mqtt_devices.MODEL ');
+$maparray=SQLSelect("SELECT * FROM (select zigbee2mqtt_devices.ID DEVID, zigbee2mqtt_devices.* from zigbee2mqtt_devices )zigbee2mqtt_devices LEFT JOIN zigbee2mqtt_devices_list ON zigbee2mqtt_devices_list.zigbeeModel like concat('%',zigbee2mqtt_devices.MODEL,'%') ");
+
+
 
 //$out['map_array']=$maparray['VALUE'];
  $nodes="";
@@ -13,8 +16,11 @@ $defaultiee=SQLSelectOne ("select * from zigbee2mqtt_devices where TITLE='bridge
 if (!$maparray[$i]['IEEEADDR']) {$idd=$defaultiee;} else {$idd=$maparray[$i]['IEEEADDR'];}
 if (!$maparray[$i]['PARRENTIEEEADDR']) {$parrentieee=$defaultiee;} else {$parrentieee=$maparray[$i]['PARRENTIEEEADDR'];}
 
-//   $nodes .= "{id: ".$idd.", font:{size:10}, size:40,   label: '".$maparray[$i]['description']."',  group: 0,  shape: 'circularImage', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', shapeProperties:{borderDashes:[5,5]},},";
-   $nodes.= "{id: ".$idd.", font:{size:10}, size:40,   label: '".$maparray[$i]['description']."',  group: 0,  shape: 'circularImage', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', },";
+//   $nodes.= "{id: ".$idd.", font:{size:10}, size:40,   label: '".$maparray[$i]['description']."',  group: 0,  shape: 'circularImage', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', },";
+   $nodes.= "{id: ".$idd.",  size:60, color:'white', font:{color:'black', size:10},  label: '".$maparray[$i]['description']."',  group: 0,  shape: 'image', image: '/templates/zigbee2mqtt/img/".$maparray[$i]['model'].".jpg', },";
+
+
+
 // if ($idd!=$defaultiee)   $edges.= "{from: $defaultiee, to: ".$idd."},";
  if ($idd!=$defaultiee)   $edges.= "{from: $parrentieee, to: ".$idd.', label: "'.$maparray[$i]['LQI'].'"},';
 }    
@@ -64,6 +70,57 @@ $out['EDGES']=$edges;
 
 
 
+$map='
+  <script type="text/javascript">
+    var nodes = null;
+    var edges = null;
+    var network = null;
+
+ 
+
+    function draw() {
+      nodes = [
+'.$nodes.'        
+
+      ];
+
+      edges = [
+'.$edges.'
+
+      ];
+
+      // create a network
+      var container = document.getElementById(\'mynetwork\');
+      var data = {
+        nodes: nodes,
+        edges: edges
+      };
+      var options = {physics:{barnesHut:{gravitationalConstant:-4000}}};
+      network = new vis.Network(container, data, options);
+    }
+  </script>
+
+<br>
+  
+
+<body onload="draw()">
+
+<div id="mynetwork"></div>
+
+<div id="info"></div>
+</body>
+
+';
+
+$out['SCRIPT']=$map;
+
+//debmes($map, 'zigbee2mqtt');
+
+
+
+
+///   $this->redirect("?tab=map");
+//   $this->redirect("?data_source=&view_mode=&id=&tab=map");
 
 ?>
 
