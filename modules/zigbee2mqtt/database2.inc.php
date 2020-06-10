b857b3cc99e29fde56c30c21f4c7b387f5f5835e
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2020-06-10 11:55:24 +0400

    Update database2.inc.php

--- modules/zigbee2mqtt/database2.inc.php
+++ modules/zigbee2mqtt/database2.inc.php
@@ -9659,6 +9659,22 @@ $par2['command_value'] = "state";
 SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 
-
+//RICI01
+$par2['zigbeeModel'] = 'RICI01';
+$par2['type'] = "plug";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "state";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "state";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 }
