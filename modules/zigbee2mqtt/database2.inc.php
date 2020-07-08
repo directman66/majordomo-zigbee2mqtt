9dd12f259de58ba406603ed5686f39fce4b74f51
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2020-07-08 16:42:02 +0400

    Update database2.inc.php

--- modules/zigbee2mqtt/database2.inc.php
+++ modules/zigbee2mqtt/database2.inc.php
@@ -9574,9 +9574,9 @@ $par2['zigbeeModel'] = 'WXCJKG13LM';
 $par2['type'] = "switch";
 $par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
 $par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
-$par2['payload_off'] = "OFF"  ;
-$par2['payload_on'] = "ON"  ;
-$par2['value_template'] = "state_left";
+$par2['payload_off'] = ""  ;
+$par2['payload_on'] = ""  ;
+$par2['value_template'] = "operation_mode";
 $par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set";
 $par2['json_attributes'] = "";
 $par2['device_class'] = "";
@@ -9584,7 +9584,7 @@ $par2['unit_of_measurement'] = "";
 $par2['brightness'] = "";
 $par2['color_temp'] = "";
 $par2['xy'] = "";
-$par2['command_value'] = "state_left";
+$par2['command_value'] = "operation_mode";
 SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 
