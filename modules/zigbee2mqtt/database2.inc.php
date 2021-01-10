e089e0207f149282e692d5d2c78943a23e33f474
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2021-01-10 21:15:53 +0400

    Update database2.inc.php

--- modules/zigbee2mqtt/database2.inc.php
+++ modules/zigbee2mqtt/database2.inc.php
@@ -10016,6 +10016,120 @@ SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 
 
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_1";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_1";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_2";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_2";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_3";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_3";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_timeout_1";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_timeout_1";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_timeout_2";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_timeout_2";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+//DIYRuZ_Flower_WS
+$par2['zigbeeModel'] = 'DIYRuZ_Flower_WS';
+$par2['type'] = "controller";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "OFF"  ;
+$par2['payload_on'] = "ON"  ;
+$par2['value_template'] = "pump_timeout_3";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "pump_timeout_3";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
+
+
+
+
 
 
 }
